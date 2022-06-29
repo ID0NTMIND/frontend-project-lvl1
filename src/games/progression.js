15869a56c4574848dacf5997563d4a46e8226f74
id0ntmind <n0thingss@yandex.ru>
2022-06-29 19:24:40 +0300

    fix progression error

--- src/games/progression.js
+++ src/games/progression.js
@@ -16,13 +16,13 @@ const getCorrectAnswer = (array, positionIndex) => String(array[positionIndex]);
 
 const progression = () => {
   const task = 'What number is missing in the progression?';
-  const firsProgressionItem = helpers.getRandomNumber(100);
-  const stepOfProgression = helpers.getRandomNumber(10);
-  const lastProgressionItem = firsProgressionItem + stepOfProgression * progressionLength;
+  const firstProgressionItem = helpers.getRandomNumber(100);
+  const stepOfProgression = helpers.getRandomNumber(10) + 1;
+  const lastProgressionItem = firstProgressionItem + stepOfProgression * progressionLength;
 
   let progressionItems = [];
   // Заполнение массива прогрессии.
-  for (let index = firsProgressionItem; index < lastProgressionItem; index += stepOfProgression) {
+  for (let index = firstProgressionItem; index < lastProgressionItem; index += stepOfProgression) {
     progressionItems = [...progressionItems, index];
   }
 
